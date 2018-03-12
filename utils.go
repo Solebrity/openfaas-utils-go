41c0a87e72095fb12944058b9ed0be7b97c3ca83
Vincent Smith <vosmith08@gmail.com>
2018-03-12 15:00:49 -0400

    added Invoke and InvokeAsync

--- utils.go
+++ utils.go
@@ -1,7 +1,9 @@
 package utils
 
 import (
+	"bytes"
 	"io/ioutil"
+	"net/http"
 	"os"
 )
 
@@ -17,4 +19,20 @@ func GetSecretValue(secretName string) (string, error) {
 		return "", err
 	}
 	return string(data), nil
+}
+
+func Invoke(function string, data []byte) ([]byte, error) {
+	resp, err := http.Post("http://gateway:8080/function/"+function, "application/octet-stream", bytes.NewReader(data))
+
+	body, _ := ioutil.ReadAll(resp.Body)
+
+	return body, err
+}
+
+func InvokeAsync(function string, data []byte, callback string) error {
+	_, err := http.Post("http://gateway:8080/async-function/"+function, "application/octet-stream", bytes.NewReader(data))
+	if err != nil {
+		return err
+	}
+	return nil
 }
